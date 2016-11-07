fe9e90991e799da3e04a6d156936d39d7e3b4f68
starsplatter <jenncd@gmail.com>
2016-11-07 16:02:22 -0500

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Exhibits
+  class Application < Rails::Application
+          config.action_mailer.default_url_options = { host: "localhost:3000", from: "noreply@example.com" }
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
