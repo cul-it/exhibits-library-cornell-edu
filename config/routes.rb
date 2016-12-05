603504022068e76d55d975d97d89cb1025d68ce5
starsplatter <jenncd@gmail.com>
2016-12-05 10:18:46 -0500

    dpla indexer

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,10 @@ Rails.application.routes.draw do
     end
   end
 
+   resources :exhibits, only: [] do
+    resources :dpla_resources, only: [:create, :update] do
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
