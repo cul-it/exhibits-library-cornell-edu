1282ef0fe5ba6fb51706cb6a82c5dd59bbb800df
starsplatter <jenncd@gmail.com>
2017-07-05 15:48:44 -0400

    all thumbnail attributes on about pages

---
+++ app/models/spotlight/about_page.rb
@@ -0,0 +1,10 @@
+module Spotlight
+  ##
+  # About pages
+  class AboutPage < Spotlight::Page
+    extend FriendlyId
+    friendly_id :title, use: [:slugged, :scoped, :finders, :history], scope: :exhibit
+
+    accepts_nested_attributes_for :thumbnail, update_only: true, reject_if: proc { |attr| attr['iiif_tilesource'].blank? }
+  end
+end
