34c81d8ea82ed7cd47ab248a1529c761b6166de4
E. Lynette Rayle <elr37@cornell.edu>
2019-03-18 08:34:41 -0400

    Add notice that confirms the actionview vulnerability was patched

--- config/initializers/formats_filter.rb
+++ config/initializers/formats_filter.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+puts 'Protecting against security vulnerability for actionview < 5.1.2.6'
+
 ActionDispatch::Request.prepend(Module.new do
   def formats
     super().select do |format|
