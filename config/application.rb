8ff35ad8f3388cfd8645ee3d560d52d37cbdd546
E. Lynette Rayle <elr37@cornell.edu>
2020-05-06 08:54:26 -0400

    clean up environment variables and add .env.example

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Exhibits
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.action_mailer.default_url_options = { host: "http://exhibits.library.cornell.edu", from: "libraryux@cornell.edu" }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,13 +6,13 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = ENV['SECRET_KEY']
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'libraryux@cornell.edu'
+  config.mailer_sender = ENV['EMAIL_FROM']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
