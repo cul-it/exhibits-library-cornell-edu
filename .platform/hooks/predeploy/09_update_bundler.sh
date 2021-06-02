e2a59bdda7995ceca2968bc56b8f7049f8dcd9d8
E. Lynette Rayle <elr37@cornell.edu>
2021-06-02 17:00:16 -0400

    update eb scripts to use new get-config; move pre/post scripts to .platform
    make pre/post deploy scripts executable

---
+++ .platform/hooks/predeploy/09_update_bundler.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+UPDATE_BUNDLER=$(/opt/elasticbeanstalk/bin/get-config environment -k UPDATE_BUNDLER)
+
+echo '***** Checking if bundler should be updated'
+if [ $UPDATE_BUNDLER = true ] ; then
+  # @example $CMD = /opt/rubies/ruby-2.7.3/bin/gem install bundler -v 2.2.18
+  # Where envinroment variables values are...
+  #   $RUBY_PATH = '/opt/rubies/ruby-2.7.3/bin/gem'
+  #   $BUNDLER_VERSION = '2.2.18'
+
+  RUBY_PATH=$(/opt/elasticbeanstalk/bin/get-config environment -k RUBY_PATH)
+  BUNDLER_VERSION=$(/opt/elasticbeanstalk/bin/get-config environment -k BUNDLER_VERSION)
+
+  CMD="$RUBY_PATH install bundler -v $BUNDLER_VERSION"
+  echo "***** Executing CMD: $CMD"
+  eval $CMD
+fi
