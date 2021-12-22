256c4def51f7bc4add9f722a9ed3024b8e3a19f4
E. Lynette Rayle <elrayle@users.noreply.github.com>
2021-12-22 17:15:15 -0500

    add WARNING to update version under `.platform/hooks`

--- .platform/confighooks/predeploy/03_mute_sidekiq.sh
+++ .platform/confighooks/predeploy/03_mute_sidekiq.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # ref: https://forums.aws.amazon.com/thread.jspa?threadID=330819
 
+# WARNING: This file is the same as `.platform/hooks/predeploy/03_mute_sidekiq.sh`.
+#   Any changes made to this file should also be made to the file under `.platform/hooks`.
+
 EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppUser)
 EB_APP_PID_DIR="/var/pids"
 
