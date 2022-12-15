92d07854b2c0da34eead69d27ca5ec9cbcdabce5
hjk54 <hjk54@cornell.edu>
2022-12-14 19:59:38 -0500

    add page type to information returned

--- app/controllers/spotlight/pages_controller.rb
+++ app/controllers/spotlight/pages_controller.rb
@@ -46,10 +46,24 @@ module Spotlight
         next unless cont.key?("data") && cont["data"].key?("item") && cont["data"]["item"].keys.length
         item = cont["data"]["item"]
         item.each do |_, v|
-          result_array.push({ "pageid" => page_id, "title" => title, "slug" => slug }) if v["id"] == item_id
+          if v["id"] == item_id
+            page_type = get_page_type(page_id)
+            result_array.push({ "pageid" => page_id, "title" => title, "slug" => slug, "pagetype" => page_type })
+          end
         end
       end
       result_array
+    end
+
+    def get_page_type(page_id)
+      page_obj = current_exhibit.pages.find(page_id)
+      page_type = "home"
+      if page_obj.about_page?
+        page_type = "about"
+      elsif page_obj.feature_page?
+        page_type = "feature"
+      end
+      page_type
     end
   end
 end
