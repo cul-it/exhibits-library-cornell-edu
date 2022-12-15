effce9e55b8a6f0e5b68a496fbb666338ccd2afc
hjk54 <hjk54@cornell.edu>
2022-12-15 12:25:29 -0500

    removing itempage methods which are now in their own controller

--- app/controllers/spotlight/pages_controller.rb
+++ app/controllers/spotlight/pages_controller.rb
@@ -16,54 +16,5 @@ module Spotlight
         format.json { render json: current_exhibit.pages.for_default_locale.published.to_json(methods: [:thumbnail_image_url]) }
       end
     end
-
-    def item_pages
-      item_id = params[:item] || ""
-      pages_json = JSON.parse(current_exhibit.pages.for_default_locale.published.to_json(methods: [:thumbnail_image_url]))
-      exhibit_id = current_exhibit.id
-      result_array = parse_pages(pages_json, item_id, exhibit_id)
-      # Now get the information
-      render json: { "results" => result_array }.to_json
-    end
-
-    def parse_pages(pages_json, item_id, exhibit_id)
-      result_array = []
-      pages_json.each do |page|
-        page_id = page["id"]
-        title = page["title"]
-        slug = page["slug"]
-        if page["content"].length.positive? && !page["content"].empty?
-          content = page["content"]
-          result_array.concat parse_page_content(content, item_id, page_id, title, slug)
-        end
-      end
-      { "exhibit_id" => exhibit_id, "pages" => result_array }
-    end
-
-    def parse_page_content(content, item_id, page_id, title, slug)
-      result_array = []
-      content.each do |cont|
-        next unless cont.key?("data") && cont["data"].key?("item") && cont["data"]["item"].keys.length
-        item = cont["data"]["item"]
-        item.each do |_, v|
-          if v["id"] == item_id
-            page_type = get_page_type(page_id)
-            result_array.push({ "pageid" => page_id, "title" => title, "slug" => slug, "pagetype" => page_type })
-          end
-        end
-      end
-      result_array
-    end
-
-    def get_page_type(page_id)
-      page_obj = current_exhibit.pages.find(page_id)
-      page_type = "home"
-      if page_obj.about_page?
-        page_type = "about"
-      elsif page_obj.feature_page?
-        page_type = "feature"
-      end
-      page_type
-    end
   end
 end
