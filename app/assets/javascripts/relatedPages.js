0220698aecb43e2e813893200e31b630b799b347
hudajkhan <hjk54@cornell.edu>
2022-12-19 15:54:42 -0500

    Copyright office request plus pages list update (#593)
    * Update README.md
    
    * adding copyright field for app with i18n string (#584)
    
    * fixing indentation for rubocop
    
    * overrides page controller to show full list of pages for non-superadmin roles. Relates to #578 and #589 (#590)
    
    * add copyright link to footer (#586)
    
    * add copyright link to footer
    
    * updating year in copyright link
    
    * Show pages for item (#591)
    
    * testing metadata portion override
    
    * checking information from item view
    
    * experimenting with solr document fields
    
    * testing information retrieval for pagelist
    
    * Adding request to get information from controller
    
    * fixing url
    
    * add page type to information returned
    
    * using page type to generate page urls
    
    * fixing error in class name
    
    * removing exhibit name from page div
    
    * moving page item list to be visible to anyone
    
    * removing itempage methods which are now in their own controller
    
    * adding functionality to implement copyright office requests (#592)

---
+++ app/assets/javascripts/relatedPages.js
@@ -0,0 +1,52 @@
+var relatedPages = {
+    onLoad:function() {
+        //Get current exhibit URL and item id and pass to method
+        var relatedPagesDiv = $("#related-pages");
+        var exhibitSlug = relatedPagesDiv.attr("exhibitslug");
+        var rootUrl = relatedPagesDiv.attr("rooturl");
+        var docid = relatedPagesDiv.attr("docid");
+        if(exhibitSlug != "" && docid != "" && rootUrl != "") {
+          relatedPages.requestPages(rootUrl, exhibitSlug, docid);
+        }
+    },
+    requestPages:function(rootUrl, exhibitSlug, docid) {
+      //Do an AJAX request for this exhibit
+      var url = rootUrl + exhibitSlug + "/itempages?item=" + docid;
+      $.ajax({
+        url : url,
+        success: function(data) {
+          console.log(data);
+          if((data != null) && ("results" in data) && ("pages" in data["results"]) && (data["results"]["pages"].length > 0)) {
+            var pages = data["results"]["pages"];
+            var pageTitles = [];
+            $.each(pages, function(i, v) {
+              pageTitles.push(relatedPages.generatePageLink(v, rootUrl, exhibitSlug));
+            });
+            $("#related-pages").html("This item is used on the following pages: " + pageTitles.join(", "));
+          }
+        },
+        error: function(xhr, status, error) {
+          console.log("Error occurred retrieving page list for " + exhibitSlug + " and item " + docid);
+          console.log(xhr.status + ":" + xhr.statusText + ":" + xhr.responseText);
+        }
+      });
+    },
+    generatePageLink(pageObject, rootUrl, exhibitSlug) {
+      var title = pageObject["title"];
+      var pageSlug = pageObject["slug"];
+      var pageType = pageObject["pagetype"];
+      var pageURL = rootUrl + exhibitSlug;
+      if(pageType == "about") {
+        pageURL += "/about/" + pageSlug;
+      } else if(pageType == "feature") {
+        pageURL += "/feature/" + pageSlug;
+      }
+      return "<a href='" + pageURL + "'>" + title + "</a>";
+    }
+};
+
+Blacklight.onLoad(function() {
+    if ( $("#related-pages").length > 0) {
+      relatedPages.onLoad();
+    }
+  });
--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module Version
-  VERSION = "v3.0.0".freeze
+  VERSION = "v3.1.0".freeze
 end
