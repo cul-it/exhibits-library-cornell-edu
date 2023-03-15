d9378afa36bb94cbd9b5671d06432b1b60ec6ae2
Christina Cortland <christina.cortland@cornell.edu>
2023-03-15 15:12:38 -0400

    Fix db migrations on circleci

--- bin/db-prepare.sh
+++ bin/db-prepare.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 # If the database exists, migrate. Otherwise setup (create and migrate)
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:environment:set RAILS_ENV=test db:create db:schema:load
 echo "Database Migration Done!"
