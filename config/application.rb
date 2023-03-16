4c7f6af21ab1f928c60a8a0209496d5490457b8a
Christina Cortland <christina.cortland@cornell.edu>
2023-03-16 13:22:37 -0400

    Revert app defaults to using rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Exhibits
   class Application < Rails::Application
+    # TODO: Match defaults to rails version 7.0 after confirming that app runs in prod
+    # https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#upgrading-from-rails-6-1-to-rails-7-0
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
