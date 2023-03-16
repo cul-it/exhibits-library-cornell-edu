2fd5b1ff6025adfd1622101ba0c73a1496afbe13
Christina Cortland <christina.cortland@cornell.edu>
2023-03-16 15:56:18 -0400

    Remove TODOs from previous upgrades

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-# TODO: UPGRADE - evaluate diffs from Spotlight base app
 class ApplicationController < ActionController::Base
   helper Openseadragon::OpenseadragonHelper
   # Adds a few additional behaviors into the application controller
@@ -6,7 +5,4 @@ class ApplicationController < ActionController::Base
   include Spotlight::Controller
 
   layout :determine_layout if respond_to? :layout
-
-  # TODO: UPGRADE - remove? following line does not exist
-  protect_from_forgery with: :exception
 end
