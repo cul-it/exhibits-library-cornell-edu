f957b8a5c278ccfe6c28c635c27c36c9254dcb14
Sarah Chintomby <sac356@cornell.edu>
2024-02-14 18:46:49 -0500

    LP-481 Render the markdown in descriptions to be rendered as clickable links on the Home and Featured Pages, for item row, carousel and slideshow elements.

---
+++ app/assets/javascripts/carousel.js
@@ -0,0 +1,7 @@
+// This script is used to stop a static carousel slideshow from preventing a link description from being clickable
+$(document).ready(function() {
+    $('.carousel-indicators .list-group-item p a').on('click', function(event) {
+      console.log('--------testing Link clicked');
+      event.stopPropagation();
+    });
+  });
--- spec/presenters/custom_field_presenter_spec.rb
+++ spec/presenters/custom_field_presenter_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe CustomFieldPresenter, type: :presenter do
   end
 
   describe '#render' do
-   
+
     # tests for the field 'spotlight_upload_description_tesim'
     context 'when field is spotlight_upload_description_tesim' do
       let(:field) { 'spotlight_upload_description_tesim' }
@@ -26,10 +26,10 @@ RSpec.describe CustomFieldPresenter, type: :presenter do
           expect(presenter.render).to eq(expected_output)
         end
       end
-      
+
       context 'when value is a Markdown link with HTML' do
-        let(:values) { ["Here is my sample description. [Google](https://www.google.com) <b>bold</b>"] }  
-       
+        let(:values) { ["Here is my sample description. [Google](https://www.google.com) <b>bold</b>"] }
+
         it 'renders a Markdown link as HTML link and escapes the HTML string' do
           expected_output = "Here is my sample description. <a href=\"https://www.google.com\">Google</a> &lt;b&gt;bold&lt;/b&gt;"
           allow(view_context).to receive(:raw).with(expected_output).and_return(expected_output)
@@ -38,8 +38,8 @@ RSpec.describe CustomFieldPresenter, type: :presenter do
       end
 
       context 'when value has both incomplete markdown and correct markdown' do
-        let(:values) { ["Here is my [sample] description. Learn about [the process](http://www.google.com)"] } 
-       
+        let(:values) { ["Here is my [sample] description. Learn about [the process](http://www.google.com)"] }
+
         it 'does renders the correct link and ignores the incorrect markdown' do
           expected_output = "Here is my [sample] description. Learn about <a href=\"http://www.google.com\">the process</a>"
           allow(view_context).to receive(:raw).with(expected_output).and_return(expected_output)
@@ -59,7 +59,7 @@ RSpec.describe CustomFieldPresenter, type: :presenter do
 
       context 'when value contains a URL not in markdown' do
         let(:values) { ['Click here(https://www.google.com)'] }
-      
+
         it 'does not render a link' do
           expected_output = 'Click here(https://www.google.com)'
           allow(view_context).to receive(:raw).with(expected_output).and_return(expected_output)
@@ -72,7 +72,7 @@ RSpec.describe CustomFieldPresenter, type: :presenter do
     context 'when field is not spotlight_upload_description_tesim' do
       let(:field) { 'spotlight_upload_category' }
       let(:values) { ['<b>bold</b>'] }
-      
+
       it 'calls super and does not render html' do
         allow_any_instance_of(Blacklight::FieldPresenter).to receive(:render).and_call_original
         expect(presenter.render).to eq('&lt;b&gt;bold&lt;/b&gt;')
