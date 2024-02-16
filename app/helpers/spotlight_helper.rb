a78bee529e07665e62ec97d9f56b1c7a16a0bb9a
sarah-cul <144364984+sarah-cul@users.noreply.github.com>
2024-02-16 16:57:33 -0500

    Lp 481 Allow markdown links in item descriptions (#651)
    * LP-481 Support for markdown links in item description

--- app/helpers/spotlight_helper.rb
+++ app/helpers/spotlight_helper.rb
@@ -11,4 +11,13 @@ module SpotlightHelper
 
     exhibit.published_at? ? l(exhibit.published_at, format: :long) : 'Unknown'
   end
+
+  def render_markdown_links(options = {})
+    values = options[:value]
+    return ''.html_safe unless values
+    safe_html = values.map do |value|
+      ERB::Util.html_escape(value).gsub(/\[([^\[]+)\]\(([^\)]+)\)/, '<a href="\2">\1</a>')
+    end.join
+    sanitize(safe_html)
+  end
 end
