f0fadc423b68a833fb635ad19edf6d95a3478b53
Christina Cortland <christina.cortland@cornell.edu>
2024-09-26 11:54:46 -0400

    Move dockerfile cmd call in scripts down

--- docker/build_test.sh
+++ docker/build_test.sh
@@ -5,5 +5,5 @@ set -e
 # Prepare DB (Migrate if exists; else Create db & Migrate)
 sh ./docker/db_prepare.sh
 
-# Run the command defined in compose.test.yaml
+# Run command if defined in the Dockerfile
 exec "$@"
