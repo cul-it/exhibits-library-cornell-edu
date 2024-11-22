d34c39d6b2d0f2b61d7c6c762ef4634eff3bd3eb
Christina Cortland <christina.cortland@cornell.edu>
2024-11-22 12:00:24 -0500

    Get .env from s3 if not available when running sidekiq docker script

--- docker/run_sidekiq.sh
+++ docker/run_sidekiq.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# Get environment variables
+WORKDIR="/exhibits"
+ENVFILE="${WORKDIR}/.env"
+if [ ! -f "$ENVFILE" ]; then
+    ruby docker/get_env.rb $RAILS_ENV $WORKDIR
+fi
+
 # Start sidekiq
 echo "Starting up sidekiq..."
 bundle exec sidekiq -e $RAILS_ENV -C config/sidekiq.yml
