d9d994544fc2fb74cd54c94f6ca6acfb68952997
Sarah Chintomby <sac356@cornell.edu>
2025-01-31 19:48:17 -0500

    - abort multiple file upload when the csv data is malformed or not UTF-8 format - abort multiple file upload when the csv headers are not correct - add new RSpec tests for the csv_upload_controller

--- app/prepends/prepended_controllers/csv_upload_controller.rb
+++ app/prepends/prepended_controllers/csv_upload_controller.rb
@@ -3,9 +3,30 @@
 module PrependedControllers::CsvUploadController
   # Overrides create method to display file name in the email message
   def create
-    csv = CSV.parse(csv_io_param, headers: true, return_headers: false).map(&:to_hash)
-    Spotlight::AddUploadsFromCsv.perform_later(csv, current_exhibit, current_user, csv_io_name)
-    flash[:notice] = t('spotlight.resources.upload.csv.success', file_name: csv_io_name)
+    begin
+      csv = CSV.parse(csv_io_param, headers: true, return_headers: false).map(&:to_hash)
+
+      # if headers are not valid, abort because metadata may be lost
+      if invalid_headers?(csv)
+        flash[:error] = t('spotlight.resources.upload.invalid_csv_headers')
+        redirect_to spotlight.admin_exhibit_catalog_path(current_exhibit)
+        return
+      end
+
+      Spotlight::AddUploadsFromCsv.perform_later(csv, current_exhibit, current_user, csv_io_name)
+      flash[:notice] = t('spotlight.resources.upload.csv.success', file_name: csv_io_name)
+    rescue CSV::MalformedCSVError => e
+      Rails.logger.error("CSV Malformed: #{e.message}")
+      flash[:error] = t('spotlight.resources.upload.malformed_non_UTF8_csv_data', error_message: e.message)
+    end
     redirect_to spotlight.admin_exhibit_catalog_path(current_exhibit)
+  end
+
+  private
+
+  def invalid_headers?(csv)
+    expected_headers = %w[url full_title_tesim spotlight_upload_description_tesim spotlight_upload_attribution_tesim spotlight_upload_date_tesim spotlight_copyright_tesim]
+    actual_headers = csv.first.keys
+    (expected_headers - actual_headers).any?
   end
 end
