271e25d60835332646b03be416c9e8344ab6af1d
Christina Cortland <christina.cortland@cornell.edu>
2025-03-17 12:53:27 -0400

    Override iiif_manifest to set annotation id for uploaded image manifests
    Remove now unnecessary spotlight js overrides for multi-image support

--- config/initializers/lib_prepends.rb
+++ config/initializers/lib_prepends.rb
@@ -1,5 +1,7 @@
 require 'migration/iiif'
+require 'iiif_manifest'
 
 Rails.application.config.to_prepare do
   Migration::IIIF.prepend PrependedLib::Iiif
+  IIIFManifest::ManifestBuilder::ImageBuilder.prepend PrependedLib::IiifManifest::ImageBuilder
 end
