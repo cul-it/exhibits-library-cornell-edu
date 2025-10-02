7fbbc708c7e6bb033c0a0006618b9d3c2b59c791
Emily Toner <ebtoner@users.noreply.github.com>
2025-10-02 15:43:39 -0400

    LP-1487 Add imageLoaderLimit to osd config to render initial image tile in embedded image viewer first

--- app/components/openseadragon_embed_component.rb
+++ app/components/openseadragon_embed_component.rb
@@ -1,4 +1,4 @@
-# Overrides Blacklight::Gallery::OpenseadragonEmbedComponent to set osd initialPage setting, default to first page
+# Overrides Blacklight::Gallery::OpenseadragonEmbedComponent to set osd imageLoaderLimit and initialPage configurations
 class OpenseadragonEmbedComponent < Blacklight::Gallery::OpenseadragonEmbedComponent
   def initialize(initial_page: 0, **kwargs)
     super
@@ -6,8 +6,23 @@ class OpenseadragonEmbedComponent < Blacklight::Gallery::OpenseadragonEmbedCompo
     @initial_page = initial_page
   end
 
+  def osd_config
+    {
+      crossOriginPolicy: false,
+      zoomInButton: "#{@id_prefix}-zoom-in",
+      zoomOutButton: "#{@id_prefix}-zoom-out",
+      homeButton: "#{@id_prefix}-home",
+      fullPageButton: "#{@id_prefix}-full-page",
+      nextButton: "#{@id_prefix}-next",
+      previousButton: "#{@id_prefix}-previous",
+      # Downloads one image at a time, so that the initial image tile is rendered before additional tiles
+      imageLoaderLimit: 1
+    }
+  end
+
   def osd_config_referencestrip
     {
+      # Set Reference Strip initial page to default to first page
       initialPage: @initial_page,
       showReferenceStrip: true,
       sequenceMode: true,
