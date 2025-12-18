dad6c36fb8d2be48213e0fccd173b067800c6710
Christina Cortland <christina.cortland@cornell.edu>
2025-12-18 13:03:53 -0500

    Use named export for rollup node-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import resolve from "@rollup/plugin-node-resolve"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 
 export default {
@@ -10,7 +10,7 @@ export default {
     sourcemap: true
   },
   plugins: [
-    resolve(),
+    nodeResolve(),
     commonjs()
   ]
 }
