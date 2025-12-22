0a217bb1466b804c97627822de10ec51117921f0
Emily Toner <ebtoner@users.noreply.github.com>
2025-12-22 09:06:51 -0500

    Rubocop fixes

--- app/prepends/prepended_resolvers/http_file_resolver/remote_file.rb
+++ app/prepends/prepended_resolvers/http_file_resolver/remote_file.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 # Based on the Module#prepend pattern in ruby.
-# Uses the to_prepare Rails hook in application.rb to inject this module to override Riiif::HttpFileResolver::RemoteFile
-
-require 'uri'
+# Uses the to_prepare Rails hook to inject this module to override Riiif::HttpFileResolver::RemoteFile
 
 module PrependedResolvers::HttpFileResolver::RemoteFile
   private
+
   def file_name
     @cache_file_name ||= ::File.join(cache_path, Digest::MD5.hexdigest(cache_url(url)) + ext.to_s)
   end
@@ -17,5 +16,5 @@ module PrependedResolvers::HttpFileResolver::RemoteFile
     cache_url.to_s
   rescue URI::Error
     url
-  end  
+  end
 end
