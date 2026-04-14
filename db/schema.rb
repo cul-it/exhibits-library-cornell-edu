03b2e92b88f8c072863cf24ac986ff866477b71b
Christina Cortland <christina.cortland@cornell.edu>
2026-04-14 14:19:49 -0400

    LP-169 - Revert db remove pw cols in case of sso rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_23_191735) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_19_171124) do
   create_table "bookmarks", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.datetime "created_at", precision: nil, null: false
     t.string "document_id"
@@ -440,12 +440,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_23_191735) do
     t.datetime "last_sign_in_at", precision: nil
     t.string "last_sign_in_ip"
     t.string "provider"
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "versions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
