0b35cc9d4fee4eb8fe43f6e6aec5e183a671d25e
Christina Cortland <christina.cortland@cornell.edu>
2026-04-14 14:18:25 -0400

    LP-169 - Retain pw date precision in case of db rollback

--- db/migrate/20260223191735_remove_database_authenticatable_columns_from_user.rb
+++ db/migrate/20260223191735_remove_database_authenticatable_columns_from_user.rb
@@ -3,9 +3,9 @@ class RemoveDatabaseAuthenticatableColumnsFromUser < ActiveRecord::Migration[8.1
     ## Recoverable
     remove_index :users, :reset_password_token, unique: true
     remove_column :users, :reset_password_token, :string
-    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :reset_password_sent_at, :datetime, precision: nil
 
     ## Rememberable
-    remove_column :users, :remember_created_at, :datetime
+    remove_column :users, :remember_created_at, :datetime, precision: nil
   end
 end
