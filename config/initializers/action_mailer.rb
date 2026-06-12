3628b297e153c657dc088684489fd09303098851
Sarah Chintomby <sac356@cornell.edu>
2026-06-12 15:57:32 -0400

    remove the unnecessary prefix for better readability

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  Exhibits::Application.config.action_mailer.default_url_options = { host: ENV['EMAIL_URL'] }
-  Exhibits::Application.config.action_mailer.default_options = { from: ENV['EMAIL_FROM'] }
-  Exhibits::Application.config.action_mailer.asset_host = ENV['EMAIL_URL']
+  config.action_mailer.default_url_options = { host: ENV['EMAIL_URL'] }
+  config.action_mailer.default_options = { from: ENV['EMAIL_FROM'] }
+  config.action_mailer.asset_host = ENV['EMAIL_URL']
 end
